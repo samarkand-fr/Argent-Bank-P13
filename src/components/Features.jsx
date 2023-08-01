65c66718a360355d147853c3c328092337735974
dina <jdina2004@gmail.com>
2023-08-01 15:27:12 +0200

    fix css issue

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -27,9 +27,10 @@ export default Features
 export const FeaturesStyle = styled.section`
   display: flex;
   flex-direction: row;
+  padding-bottom: 3.5rem;
   justify-content: space-between;
   @media (max-width: 720px) {
     flex-direction: column;
-    padding-bottom: 2.5rem;
+    padding-bottom: 5.5rem;
   }
 `
